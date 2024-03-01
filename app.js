d3adcc1c384a9c459bfe71c05a466a764da951a4
SUBHIN-TM <subhintm2018@gmail.com>
2024-03-01 10:45:07 +0530

    react element

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-
+//!REACT ELEMENT
 // const heading= React.createElement("div",{id:"trys"},"welcome by parcel")
-const jsxHeading=<h1 id="trys">weclome using jsx</h1>
-            
+const jsxHeading=<h1 id="trys" className="classgiven">weclome using jsx</h1>
+//*babel is compling this code to react for understandable jsx            
+
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(jsxHeading)
+
