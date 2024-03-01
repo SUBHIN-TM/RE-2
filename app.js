faaca1f302b43a318ede5e674adc3ee55ec3e3b4
SUBHIN-TM <subhintm2018@gmail.com>
2024-03-01 10:24:52 +0530

    jsx replaced syntax for create-element

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-/* <div id="parent">
-<div id="child 1">c1</div>
-<div id="child 2">c2</div>
-</div> 
-<div id="parent">
-<div id="child 1">c1</div>
-<div id="child 2">c2</div>
-</div> 
-*/
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
 
-const heading= [React.createElement("div",{id:"parent "},[React.createElement("div",{id:"child 1"},"Hi"),React.createElement("div",{id:"child 2"},"c2")]),
-               React.createElement("div",{id:"parent2"},[React.createElement("div",{id:"child 3"},"Welcome"),React.createElement("div",{id:"child 4"},"c2")])]
-
+// const heading= React.createElement("div",{id:"trys"},"welcome by parcel")
+const jsxHeading=<h1 id="trys">weclome using jsx</h1>
+            
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(jsxHeading)
